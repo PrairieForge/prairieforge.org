72f0202be21c9425fecf6ac515117ce4f21375d4
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-05 23:55:36 -0600

    Fix exposed secret in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import { loadEnv } from "vite";
+
+// For Local Dev, set NGROK_HOST from .env
+const { NGROK_HOST } = loadEnv(process.env, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 		server: {
-			allowedHosts: ["edgar-persuasive-hudson.ngrok-free.dev"],
+			allowedHosts: [NGROK_HOST],
 		},
 	},
 
