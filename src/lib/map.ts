39be861864e43a501f932adb3f6f84eac883a42f
pf_admin <247836683+PrairieForge@users.noreply.github.com>
2026-07-05 07:16:52 -0500

    update deps and fix map marker

--- src/lib/map.ts
+++ src/lib/map.ts
@@ -42,9 +42,18 @@ const createPopupContent = ({
       `;
 };
 
+const customIcon = L.icon({
+	iconUrl: '/map-marker.svg',
+	iconSize: [38, 95], // size of the icon
+	iconAnchor: [19, 94], // point of the icon which will correspond to marker's location
+	popupAnchor: [0, -76], // point from which the popup should open relative to the iconAnchor
+});
+
 export const addMarker = (item: CollectionEntry<'churches'>) => {
 	const gps: [number, number] = JSON.parse(item.data.gps).coordinates;
 	const coords: [number, number] = [gps[1], gps[0]];
 
-	return L.marker(coords).bindPopup(createPopupContent(item.data));
+	return L.marker(coords, { icon: customIcon }).bindPopup(
+		createPopupContent(item.data),
+	);
 };
